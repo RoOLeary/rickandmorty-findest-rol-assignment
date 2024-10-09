c12a3de0577705c09e7d183546da3efafcef3400
Ronan O'Leary <roleary81@gmail.com>
2024-10-09 22:52:21 +0200

    update mobile views to look semi decent

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,6 +6,7 @@ const Landing = () => {
     <div className="landing">
         <a href="/findest-explorer">
             <div className="portal rotating"></div>
+            
             <div className="title">Ro <span>and </span>Findest</div>
             <div className="title middle"> Ro <span>and </span>Findest</div>
             <div className="title bottom"> Ro <span>and </span>Findest</div>
